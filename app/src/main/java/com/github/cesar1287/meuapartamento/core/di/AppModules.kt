e12c8134204efb0be879c9ee37a6b24ddb59c272
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-08-08 15:06:44 -0300

    update di modules

--- app/src/main/java/com/github/cesar1287/meuapartamento/core/di/AppModules.kt
+++ app/src/main/java/com/github/cesar1287/meuapartamento/core/di/AppModules.kt
@@ -1,13 +1,16 @@
 package com.github.cesar1287.meuapartamento.core.di
 
-import com.github.cesar1287.meuapartamento.features.MainBusiness
-import com.github.cesar1287.meuapartamento.features.MainViewModel
+import com.github.cesar1287.meuapartamento.features.main.business.MainBusiness
+import com.github.cesar1287.meuapartamento.features.main.viewmodel.MainViewModel
 import com.github.cesar1287.meuapartamento.core.repository.MainRepository
+import com.github.cesar1287.meuapartamento.features.payment.business.PaymentBusiness
+import com.github.cesar1287.meuapartamento.features.payment.viewmodel.PaymentViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val businessModule = module {
     single { MainBusiness() }
+    single { PaymentBusiness() }
 }
 
 val repositoryModule = module {
@@ -15,5 +18,6 @@ val repositoryModule = module {
 }
 
 val viewModelModule = module {
-    viewModel { MainViewModel() }
+    viewModel { MainViewModel(get()) }
+    viewModel { PaymentViewModel(get()) }
 }
